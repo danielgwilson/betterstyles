63bb04afffef53f8b8da404cef68521ef8df60eb
danielgwilson <daniel@danielgwilson.com>
2018-11-07 11:49:27 -0800

    Working UI with cards and styles, no functionality

--- betterstyles/src/App.js
+++ betterstyles/src/App.js
@@ -1,28 +1,45 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
+// import logo from "./logo.svg";
 import "./App.css";
 import "./StyleCard";
 import StyleCard from "./StyleCard";
 
 class App extends Component {
     render() {
+        const styleCards = [
+            {
+                name: "normal",
+                properties: { fontSize: 11, fontFamily: "roboto" }
+            },
+            {
+                name: "header",
+                properties: { fontSize: 24, fontFamily: "roboto" }
+            },
+            {
+                name: "subheader",
+                properties: { fontSize: 18, fontFamily: "roboto" }
+            },
+            {
+                name: "bold",
+                properties: { fontSize: 11, fontFamily: "roboto" }
+            },
+            {
+                name: "italic",
+                properties: { fontSize: 11, fontFamily: "roboto" }
+            }
+        ];
+        const styleCardsJSX = styleCards.map(function(styleCard, index) {
+            return (
+                <StyleCard
+                    key={index}
+                    name={styleCard.name}
+                    properties={styleCard.properties}
+                />
+            );
+        });
         return (
             <div className="App">
-                <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo" />
-                    <p>
-                        Edit <code>src/App.js</code> and save to reload.
-                    </p>
-                    <a
-                        className="App-link"
-                        href="https://reactjs.org"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        Learn React
-                    </a>
-                    <StyleCard />
-                </header>
+                <div className="App-body">{styleCardsJSX}</div>
             </div>
         );
     }
--- betterstyles/src/StyleCard.js
+++ betterstyles/src/StyleCard.js
@@ -4,16 +4,17 @@ import { withStyles } from "@material-ui/core/styles";
 
 import Card from "@material-ui/core/Card";
 import CardActionArea from "@material-ui/core/CardActionArea";
-import CardActions from "@material-ui/core/CardActions";
+// import CardActions from "@material-ui/core/CardActions";
 import CardContent from "@material-ui/core/CardContent";
-import Button from "@material-ui/core/Button";
+// import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
 
 import "typeface-roboto";
 
 const styles = {
     card: {
-        minWidth: 275
+        minWidth: 275,
+        marginBottom: 8
     },
     bullet: {
         display: "inline-block",
@@ -25,50 +26,69 @@ const styles = {
     },
     pos: {
         marginBottom: 12
+    },
+    properties: {
+        fontSize: 10
+    },
+    name: {
+        fontSize: "11pt",
+        fontFamily: "Calibri"
     }
 };
 
 function StyleCard(props) {
     const { classes } = props;
-    const bull = <span className={classes.bullet}>•</span>;
+    const { name } = props;
+
+    // generate style overrides string from properties
+    const { properties } = props;
+    let styleString = "";
+    for (const [key, value] of Object.entries(properties)) {
+        styleString = `${styleString} ${key}: ${value}, `;
+    }
+    // remove trailing comma and space
+    styleString = `${styleString.slice(0, -2)}`;
+    console.log(styleString);
+    // const bull = <span className={classes.bullet}>•</span>;
 
     return (
         <Card className={classes.card}>
             <CardActionArea>
                 <CardContent>
-                    <Typography
+                    {/* <Typography
                         className={classes.title}
                         color="textSecondary"
                         gutterBottom
                     >
                         Word of the Day
+                    </Typography> */}
+                    <Typography className={classes.name} style={properties}>
+                        {name}
                     </Typography>
-                    <Typography variant="h5" component="h2">
-                        be
-                        {bull}
-                        nev
-                        {bull}o{bull}
-                        lent
+                    <Typography
+                        className={classes.properties}
+                        color="textSecondary"
+                    >
+                        {styleString}
                     </Typography>
-                    <Typography className={classes.pos} color="textSecondary">
-                        adjective
-                    </Typography>
-                    <Typography component="p">
+                    {/* <Typography component="p">
                         well meaning and kindly.
                         <br />
                         {'"a benevolent smile"'}
-                    </Typography>
+                    </Typography> */}
                 </CardContent>
             </CardActionArea>
-            <CardActions>
+            {/* <CardActions>
                 <Button size="small">Learn More</Button>
-            </CardActions>
+            </CardActions> */}
         </Card>
     );
 }
 
 StyleCard.propTypes = {
-    classes: PropTypes.object.isRequired
+    classes: PropTypes.object.isRequired,
+    name: PropTypes.string.isRequired,
+    properties: PropTypes.object.isRequired
 };
 
 export default withStyles(styles)(StyleCard);
